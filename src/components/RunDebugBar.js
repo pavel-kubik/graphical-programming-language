f85e50c23203e3f6c79354b1d2b13b96b34815a2
Pavel <roigoci@gmail.com>
2022-12-13 11:35:38 +0100

    Add debugger

--- src/components/RunDebugBar.js
+++ src/components/RunDebugBar.js
@@ -1,11 +1,14 @@
 import './RunDebugBar.css';
 
-const RunDebugBar = ({ runHandler, mode, handleChangeMode }) => {
+const RunDebugBar = ({ runHandler, debugHandler, mode, handleChangeMode }) => {
   return (
     <div className="RunDebugBar">
       <div onClick={handleChangeMode}>{mode}</div>
       <div className="Run" onClick={runHandler}>
         Run
+      </div>
+      <div className="Run" onClick={debugHandler}>
+        Debug
       </div>
     </div>
   );
